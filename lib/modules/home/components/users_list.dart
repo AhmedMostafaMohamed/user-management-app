a659a68e75e51edde6bd2ae2e3cd9e4c262c01ba
AhmedMostafaMohamed <ahmedmostafa1on1@gmail.com>
2023-08-01 15:55:09 +0200

    google sign in

---
+++ lib/modules/home/components/users_list.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import '../../../data/models/user.dart';
+
+class UsersList extends StatefulWidget {
+  final List<User> users;
+  const UsersList({Key? key, required this.users}) : super(key: key);
+
+  @override
+  State<UsersList> createState() => _UsersListState();
+}
+
+class _UsersListState extends State<UsersList> {
+  List<User> filteredUsers = [];
+
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    filteredUsers = widget.users;
+  }
+
+  void filterUsers(String searchTerm) {
+    setState(() {
+      if (searchTerm.isEmpty) {
+        filteredUsers = widget.users;
+      } else {
+        filteredUsers = widget.users
+            .where((user) =>
+                user.email.toLowerCase().contains(searchTerm.toLowerCase()) ||
+                user.role.name.toLowerCase().contains(searchTerm.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
+  void viewUserDetails(User user) {
+    Navigator.of(context).pushNamed('/user-details', arguments: user);
+  }
+
+  Color getRoleColor(Role role) {
+    if (role == Role.admin) {
+      return Colors.blue; // Use your preferred color for 'Admin' role
+    } else {
+      return Colors.green; // Use your preferred color for 'User' role
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: TextField(
+              onChanged: filterUsers,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                hintText: 'Enter search keyword',
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor: Theme.of(context).inputDecorationTheme.fillColor,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 12.0,
+                  horizontal: 16.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                    width: 2.0,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).disabledColor,
+                    width: 1.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: filteredUsers.length,
+            itemBuilder: (context, index) {
+              final user = filteredUsers[index];
+              final roleColor = getRoleColor(user.role);
+
+              return GestureDetector(
+                onTap: () {
+                  viewUserDetails(user);
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 4.0,
+                    horizontal: 16.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        offset: const Offset(0, 2),
+                        blurRadius: 4.0,
+                      ),
+                    ],
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              user.email,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8.0),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 4.0,
+                                horizontal: 8.0,
+                              ),
+                              decoration: BoxDecoration(
+                                color: roleColor,
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: Text(
+                                user.role.name,
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 16.0),
+                      const Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
